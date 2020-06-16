f959a773a239ab4e8af0e91ac037a9c257c4ab80
arcanar7 <arcanar7@gmail.com>
2020-06-16 23:31:26 +0400

    RS-10 feat(miniGamesPage): add cards

--- src/views/MiniGamesPage.vue
+++ src/views/MiniGamesPage.vue
@@ -1,30 +1,85 @@
 <template>
   <div class="mini-games">
-    <div class="mini-games__card">1</div>
-    <div class="mini-games__card">2</div>
-    <div class="mini-games__card">3</div>
-    <div class="mini-games__card">4</div>
-    <div class="mini-games__card">5</div>
-    <div class="mini-games__card">6</div>
+    <div class="mini-games__card mini-games__card_speak-it">
+      <IconBase width="57px" height="71px">
+        <IconMegaphone />
+      </IconBase>
+      <h2>Speak It!</h2>
+      <router-link to="/english-puzzle">Play!</router-link>
+    </div>
+    <div class="mini-games__card mini-games__card_puzzle">2</div>
+    <div class="mini-games__card mini-games__card_savannah">3</div>
+    <div class="mini-games__card mini-games__card_audiocall">4</div>
+    <div class="mini-games__card mini-games__card_sprint">5</div>
+    <div class="mini-games__card mini-games__card_my-game">6</div>
   </div>
 </template>
 
 <script>
-export default {};
+import IconBase from '@/components/IconBase.vue';
+import IconMegaphone from '@/components/icons/IconMegaphone.vue';
+// import IconPuzzle from '@/components/icons/IconPuzzle.vue';
+// import IconSavannah from '@/components/icons/IconSavannah.vue';
+// import IconVideoCall from '@/components/icons/IconVideoCall.vue';
+// import IconRunning from '@/components/icons/IconRunning.vue';
+// import IconPoker from '@/components/icons/IconPoker.vue';
+
+export default {
+  name: 'MiniGamesPage',
+  components: {
+    IconBase,
+    IconMegaphone,
+    // IconPuzzle,
+    // IconSavannah,
+    // IconVideoCall,
+    // IconRunning,
+    // IconPoker,
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 .mini-games {
   display: flex;
   flex-wrap: wrap;
-  justify-content: space-between;
+  justify-content: center;
   width: 100%;
   height: 100%;
-  padding: 40px 16px;
+  padding-top: 24px;
 }
 
 .mini-games__card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
   width: 358px;
   height: 358px;
+  margin: 16px;
+  color: $color-white;
+
+  &_speak-it {
+    background-color: $color-dodger-blue;
+  }
+
+  &_puzzle {
+    background-color: $color-wild-watermelon;
+  }
+
+  &_savannah {
+    background-color: $color-shuttle-gray;
+  }
+
+  &_audiocall {
+    background-color: $color-golden-dream;
+  }
+
+  &_sprint {
+    background-color: $color-malibu;
+  }
+
+  &_my-game {
+    background-color: $color-dodger-blue;
+  }
 }
 </style>
