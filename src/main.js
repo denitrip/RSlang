6d15e442414b17877bd748a55b7a2d2822bd8194
Dzianis Puchko <dixon.ltd50@gmail.com>
2020-06-16 22:18:25 +0300

    RS-31 feat(settings): add vue-bootstrap

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue';
 import router from '@/router';
 import store from '@/store';
 import App from '@/App.vue';
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false;
 
