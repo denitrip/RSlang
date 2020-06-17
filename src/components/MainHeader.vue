deb386bd7b9fd5515000017373342646237ace50
dajiquwe <60528148+dajiquwe@users.noreply.github.com>
2020-06-17 23:24:24 +0300

    RS-3 feat(MainHeader): implement MainHeader (#40)
    * RS-3 feat(MainHeader): add MainHeader

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>Main Header</div>
+  <header class="container">
+    <div class="container-label">
+      <h2>Dictionary</h2>
+    </div>
+    <div class="container user-section">
+      <img src="assets/images/notifications_image.png" alt="#" />
+      <img src="assets/images/settings_image.png" alt="#" />
+      <div class="user-details">
+        <div class="user-name">
+          Ivan Ivanov
+        </div>
+        <div class="user-level">
+          Level 3
+        </div>
+      </div>
+      <div class="user-avatar"><img src="assets/images/user_image.png" alt="#" /></div>
+    </div>
+  </header>
 </template>
 
 <script>
@@ -9,4 +26,47 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.container {
+  margin-left: 40px;
+  margin-right: 40px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+img {
+  height: 20px;
+  width: 20px;
+  margin-left: 10px;
+}
+
+.container-label h2 {
+  display: inline-block;
+  font-weight: 900;
+  margin: 0;
+  padding: 0;
+  color: #483d8b;
+}
+
+.user-section {
+  margin-left: 10px;
+}
+
+.user-details {
+  display: block;
+  align-items: center;
+  margin-left: 10px;
+}
+
+.user-level {
+  font-size: 12px;
+  opacity: 50%;
+}
+
+.user-avatar img {
+  border-radius: 50%;
+  height: 30px;
+  width: 30px;
+}
+</style>
