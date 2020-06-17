207bf8c849953527a8dbdcfd7f9c3416e7edee6a
arcanar7 <arcanar7@gmail.com>
2020-06-17 22:47:42 +0400

    RS-23 feat(loginForm): add validation email

---
+++ src/components/AppSpinner.vue
@@ -0,0 +1,116 @@
+<template functional>
+  <div class="lds-spinner lds-spinner_small">
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppSpinner',
+};
+</script>
+
+<style lang="scss">
+.lds-spinner {
+  position: relative;
+  display: inline-block;
+  color: official;
+
+  div {
+    animation: lds-spinner 1.2s linear infinite;
+
+    &::after {
+      position: absolute;
+      display: block;
+      content: ' ';
+      background: $color-onahau;
+      border-radius: 20%;
+    }
+  }
+}
+
+.lds-spinner_small {
+  @include spinner(23px);
+}
+
+.lds-spinner div:nth-child(1) {
+  transform: rotate(0deg);
+  animation-delay: -1.1s;
+}
+
+.lds-spinner div:nth-child(2) {
+  transform: rotate(30deg);
+  animation-delay: -1s;
+}
+
+.lds-spinner div:nth-child(3) {
+  transform: rotate(60deg);
+  animation-delay: -0.9s;
+}
+
+.lds-spinner div:nth-child(4) {
+  transform: rotate(90deg);
+  animation-delay: -0.8s;
+}
+
+.lds-spinner div:nth-child(5) {
+  transform: rotate(120deg);
+  animation-delay: -0.7s;
+}
+
+.lds-spinner div:nth-child(6) {
+  transform: rotate(150deg);
+  animation-delay: -0.6s;
+}
+
+.lds-spinner div:nth-child(7) {
+  transform: rotate(180deg);
+  animation-delay: -0.5s;
+}
+
+.lds-spinner div:nth-child(8) {
+  transform: rotate(210deg);
+  animation-delay: -0.4s;
+}
+
+.lds-spinner div:nth-child(9) {
+  transform: rotate(240deg);
+  animation-delay: -0.3s;
+}
+
+.lds-spinner div:nth-child(10) {
+  transform: rotate(270deg);
+  animation-delay: -0.2s;
+}
+
+.lds-spinner div:nth-child(11) {
+  transform: rotate(300deg);
+  animation-delay: -0.1s;
+}
+
+.lds-spinner div:nth-child(12) {
+  transform: rotate(330deg);
+  animation-delay: 0s;
+}
+
+@keyframes lds-spinner {
+  0% {
+    opacity: 1;
+  }
+
+  100% {
+    opacity: 0;
+  }
+}
+</style>
--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,15 +1,28 @@
 <template>
-  <form class="login">
+  <form class="login" novalidate>
     <h4 class="h4 wrap">Log in</h4>
     <div class="wrap">
-      <label for="login" class="email__wrong_color">Email or login</label>
+      <label for="login" :class="{ email__wrong_color: isValidErrorEmail }">Email or login</label>
       <input
-        class="input email__wrong_border email__correct"
+        class="input"
+        :class="{
+          email__wrong_border: isValidErrorEmail,
+          email__correct: !isInvalidEmail,
+        }"
         type="email"
         name="login"
         id="login"
+        v-model="email.title"
+        @blur="email.touch()"
       />
-      <div class="email__wrong email__wrong_color">The e-mail adress is incomplete.</div>
+      <span v-if="isValidErrorEmail" class="email__wrong email__wrong_color">
+        <template v-if="!isRequiredEmail">
+          Required.
+        </template>
+        <template v-else-if="!isEmail">
+          The e-mail adress is incomplete.
+        </template>
+      </span>
     </div>
     <div class="wrap">
       <label for="password">Password</label>
@@ -21,27 +34,90 @@
       ></span>
     </div>
     <button type="submit" class="btn btn-login wrap btn-error">
-      Log in <span class="btn-arrow"></span>
+      Log in
+      <AppSpinner v-if="isAuthLoading"></AppSpinner>
+      <span v-else class="btn-arrow"></span>
     </button>
     <div class="signup wrap">
-      <label for="signup">Don't have account? </label>
-      <a href="#" id="signup">Sign Up</a>
+      <label for="signup">Don't have account?</label>
+      <a href="#" id="signup"> Sign Up</a>
     </div>
   </form>
 </template>
 
 <script>
+import AppSpinner from '@/components/AppSpinner.vue';
+import ValidField from '@/helpers/auth.helper';
+
 export default {
   name: 'LoginForm',
-  components: {},
+  components: {
+    AppSpinner,
+  },
   data() {
     return {
+      isAuthLoading: false,
       isPasswordShow: false,
+      email: new ValidField(null),
+      password: new ValidField(null),
+      repeatPassword: new ValidField(null),
+      emailRegex: /(^$|^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$)/,
+      passwordRegex: /(?=.*[0-9])(?=.*[-+_@$!%*?&#.,;:[\]{}])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z-+_@$!%*?&#.,;:[\]{}]{8,}/g,
     };
+  },
+  computed: {
+    isRequiredEmail() {
+      return this.isRequired(this.email.title);
+    },
+    isRequiredPassword() {
+      return this.isRequired(this.password.title);
+    },
+    isRequiredRepeatPassword() {
+      return this.isRequired(this.repeatPassword.title);
+    },
+    isEmail() {
+      return this.email.title ? this.emailRegex.test(this.email.title) : false;
+    },
+    isPassword() {
+      return this.password.title ? this.passwordRegex.test(this.password.title) : false;
+    },
+    isMinLength() {
+      return this.password.title.length >= 8;
+    },
+    isSameAs() {
+      return this.password.title === this.repeatPassword.title;
+    },
+    isInvalidEmail() {
+      return !this.isRequiredEmail || !this.isEmail;
+    },
+    isInvalidPassword() {
+      return !this.isRequiredPassword || !this.isMinLength || !this.isPassword;
+    },
+    isInvalidRepeatPassword() {
+      return !this.isRequiredRepeatPassword || !this.isSameAs;
+    },
+    isValidErrorEmail() {
+      return this.email.touched && this.isInvalidEmail;
+    },
+    isValidErrorPassword() {
+      return this.password.touched && this.isInvalidPassword;
+    },
+    isValidErrorRepeatPassword() {
+      return this.repeatPassword.touched && this.isInvalidRepeatPassword;
+    },
+    isLoginValid() {
+      return this.isInvalidEmail || this.isInvalidPassword;
+    },
+    isRegistrationValid() {
+      return this.isInvalidEmail || this.isInvalidPassword || this.isInvalidRepeatPassword;
+    },
   },
   methods: {
     showPassword() {
       this.isPasswordShow = !this.isPasswordShow;
+    },
+    isRequired(field) {
+      return !!field;
     },
   },
 };
@@ -51,11 +127,11 @@ export default {
 .login {
   display: flex;
   flex-direction: column;
-  max-width: 315px;
+  max-width: 429px;
   padding: 60px 57px;
-  background: #fffefe;
+  background: $color-rose-white;
   border-radius: 20px;
-  box-shadow: 1px 5px 14px rgba(0, 0, 0, 0.1);
+  box-shadow: 1px 5px 14px $box-shadow-one-color;
 
   .wrap {
     position: relative;
@@ -80,6 +156,7 @@ export default {
     font-size: 14px;
     line-height: 16px;
     color: $color-mine-shaft;
+    background-color: transparent;
     border: none;
     border-bottom: 1px solid $color-dodger-blue;
     outline: none;
@@ -123,6 +200,7 @@ export default {
 
   .password__eye_close {
     bottom: 1px;
+    cursor: pointer;
     background-image: url('~@/assets/img/password-eye-close.png');
     background-repeat: no-repeat;
     background-size: 18px;
@@ -138,7 +216,7 @@ export default {
   .btn {
     font-size: 20px;
     line-height: 23px;
-    color: #ffffff;
+    color: $color-white;
     cursor: pointer;
     background: $color-dodger-blue;
     border: none;
@@ -165,5 +243,9 @@ export default {
     cursor: default;
     background-color: $color-malibu;
   }
+}
+
+.signup {
+  text-align: center;
 }
 </style>
