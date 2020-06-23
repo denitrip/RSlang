d08c24f21d0bca06ed1c88dd532a24c79b5b5730
RemyJS <a.king92@mail.ru>
2020-06-23 11:41:07 +0300

    have to change commit in RS-80

--- src/views/LearningPage.vue
+++ src/views/LearningPage.vue
@@ -1,6 +1,14 @@
 <template>
-  <div class="learning__wrapper">
-    <learning-card  englishWord="water" russianWord="вода" transcription="[ˈˈwɔtər]" leadingQuestion="Drink to quench your thirst" sentanceWithWord="I like to drink water"></learning-card>
+  <div class="learning">
+    <div class="learning__header">
+      <h1><span>Hello.</span> Let’s start learning!</h1>
+      <p>
+        On this page, you can monitor your progress and select the desired set of words to learn,
+        for example, <b>“New words”</b>, <b>“Repetition”</b> or <b>“All words”</b>. Good luck!
+      </p>
+    </div>
+    <div class="learning__today"></div>
+    <div class="learning__category"></div>
   </div>
 </template>
 
@@ -10,6 +18,7 @@ import LearningCard from '../components/LearningCard.vue';
 export default {
   name: 'LearningPage',
   components: {
+    // eslint-disable-next-line vue/no-unused-components
     LearningCard,
   },
 };
@@ -17,13 +26,32 @@ export default {
 
 <style scoped lang="scss">
 .learning {
-  &__wrapper {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-around;
-    max-width: $content-width;
-    padding: 20px 0 30px 0;
-    margin: auto;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  width: 100%;
+  height: 100%;
+  background-color: $color-catskill-white;
+
+  &__header {
+    padding: 25px 69px 48px;
+
+    h1 {
+      font-size: 56px;
+      font-weight: 900;
+      line-height: 63px;
+      color: $color-pickled-bluewood;
+
+      span {
+        color: $color-golden-dream;
+      }
+    }
+
+    p {
+      margin-top: 15px;
+      font-size: 20px;
+      color: $color-shuttle-gray;
+    }
   }
 }
 </style>
