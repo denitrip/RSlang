221f2fbc0ada6f8a0437114f59c3ea060f37133b
dajiquwe <djangar_z@mail.ru>
2020-06-23 18:05:19 +0300

    RS-61 feat(notification): add notification menu functionality

--- src/store/index.js
+++ src/store/index.js
@@ -7,6 +7,7 @@ import Settings from '@/store/modules/settings';
 import EnglishPuzzle from '@/store/modules/englishPuzzle';
 import Savannah from '@/store/modules/savannah';
 import LongTermStatistic from '@/store/modules/longTermStatistic';
+import Notification from '@/store/modules/notification';
 
 Vue.use(Vuex);
 
@@ -19,5 +20,6 @@ export default new Vuex.Store({
     EnglishPuzzle,
     Savannah,
     LongTermStatistic,
+    Notification,
   },
 });
